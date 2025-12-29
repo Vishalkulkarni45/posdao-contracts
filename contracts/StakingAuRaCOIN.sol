b891d61689f8917d09650ddb23226059b8822f45
vishal <vishalkoolkarni0045@gmail.com>
2025-12-29 19:39:49 +0530

    feat: blockreward and staking contract for native coins

---
+++ contracts/StakingAuRaCOIN.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.5.10;
+
+//import "./base/StakingAuRaTokens.sol";
+import "./base/StakingAuRaCoins.sol";
+
+
+contract StakingAuRa is StakingAuRaCoins {}
+// Uncomment this line and comment out the above one
+// if staking in native coins is needed instead of staking in tokens:
+// contract StakingAuRa is StakingAuRaCoins {}
